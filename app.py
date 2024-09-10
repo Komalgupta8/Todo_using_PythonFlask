301707cbaf426754c1f57ea9ab3609f53188166a
Komalgupta8 <komalguptagupta234@gmail.com>
2024-09-10 13:33:49 +0530

    intial commit

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask , render_template , request , redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+# create database
+
+class Todo(db.Model):
+   sno=db.Column(db.Integer , primary_key=True)
+   title=db.Column(db.String (200), nullable=False)
+   dsc=db.Column(db.String(500), nullable=False)
+   date_now = db.Column(db.DateTime, default=datetime.utcnow)
+#    action=db.Column(db.)
+
+
+    # koi cheez dekhni hai toh yha se dekh skte hai is method se
+   def __repr__(self) -> str:
+      return f"{self.sno}-{self.title}"
+   
+
+#  jab bhi hum koi post karte hai toh hume route me methods ki list banakr likhna pdta hai
+
+@app.route("/" , methods=['get' , 'POST']) 
+def hello_world():
+    if request.method == 'POST':
+        title = request.form['title']
+        dsc = request.form['dsc']
+
+        todo = Todo(title=title, dsc=dsc)
+        db.session.add(todo)
+        db.session.commit()
+    
+    # If the method is GET or after POST processing, render the template
+    show_all = Todo.query.all()
+    return render_template("index.html", show_all=show_all)
+
+
+# Route to display all todos
+@app.route("/show")
+def products():
+    show_all=Todo.query.all()
+    print(show_all)
+    return 'this is products page'
+
+# Route to delete a todo
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()  # Fetch the todo item
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+
+@app.route("/update/<int:sno>", methods=['GET', 'POST'])
+def update(sno):
+    todo = Todo.query.filter_by(sno=sno).first_or_404()  # Fetch the todo item
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.dsc = request.form['dsc']
+        db.session.commit()
+        return redirect("/")
+    return render_template("update.html", todo=todo)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # This creates the tables when the script is run
+    app.run(debug=True, port=8000)
